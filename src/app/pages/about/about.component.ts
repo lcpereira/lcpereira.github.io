23b3dfe4572ee72d44000bafecb4342fccb11d0a
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2021-07-27 15:13:25 -0300

    feat(resume-pdf): added download resume pdf

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'ngx-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
-  constructor() {}
+export class AboutComponent {
+  constructor(private translocoService: TranslocoService) {}
 
-  ngOnInit(): void {}
+  get language(): string {
+    return this.translocoService.getActiveLang();
+  }
 }
