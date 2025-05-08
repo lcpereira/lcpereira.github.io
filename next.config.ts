a49022e9152ae5bac304459d8e83a54c59663e75
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2025-05-08 14:18:20 -0300

    chore: changes to test perform

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  swcMinify: false,
   headers: async () => {
     return Promise.resolve([
       {
