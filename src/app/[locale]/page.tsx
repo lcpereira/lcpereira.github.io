c7bc32b07903a3f4e26e7418d3ec2b23ef853c3f
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2025-05-08 14:30:27 -0300

    chore: changes to test perform

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 import HeroSection from '@/components/HeroSection';
-import RecentPosts from '@/components/RecentPosts';
-import { getPosts } from '@/lib/getPosts';
-import AboutSection from '@/components/AboutSection';
-import SkillsSection from '@/components/SkillsSection';
+// import RecentPosts from '@/components/RecentPosts';
+// import { getPosts } from '@/lib/getPosts';
+// import AboutSection from '@/components/AboutSection';
+// import SkillsSection from '@/components/SkillsSection';
 
 type Props = {
   params: Promise<{ locale: string }>;
@@ -10,14 +10,15 @@ type Props = {
 
 export default async function HomePage({ params }: Props) {
   const { locale } = await params;
-  const posts = await getPosts(locale);
+  console.log(locale);
+  // const posts = await getPosts(locale);
 
   return (
     <main className="max-w-4xl mx-auto px-4 py-8">
       <HeroSection />
-      <AboutSection />
-      <SkillsSection />
-      <RecentPosts posts={posts.slice(0, 3)} />
+        {/* <AboutSection />
+        <SkillsSection />
+        <RecentPosts posts={posts.slice(0, 3)} /> */}
     </main>
   );
 }
