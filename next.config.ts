95921dfdcc0ebf08a048ecbf815fcd570bcc2ab8
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2025-05-12 09:41:14 -0300

    fix: resolve load page cache

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,26 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  headers: async () => {
-    return Promise.resolve([
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ]);
-  }
+  headers: async () => [
+    {
+      source: '/_next/static/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable',
+        },
+      ],
+    },
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-cache',
+        },
+      ],
+    },
+  ],
 };
 const withNextIntl = createNextIntlPlugin();
 
