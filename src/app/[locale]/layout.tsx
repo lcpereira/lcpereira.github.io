beba8b3ea66ecf34400754e5a367ece2f0106e9d
Leandro Carlos Pereira <lcpereira.dev@gmail.com>
2025-05-12 09:14:21 -0300

    fix: resolve load page cache

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -67,7 +67,7 @@ export async function generateMetadata({
   };
 }
 
-export const dynamic = 'force-static';
+// export const dynamic = 'force-static';
 
 export default async function RootLayout({ children, params }: Props) {
   const { locale } = await params;
